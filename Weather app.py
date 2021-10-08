3ccfcf50fd444b4b0539c07c3e7003acf62d6c0e
Kunal D <88197438+kunald9960@users.noreply.github.com>
2021-10-09 00:08:45 +0530

    Update Weather app.py

--- Weather app.py
+++ Weather app.py
@@ -1,38 +1,68 @@
+import tkinter as tk
+from tkinter.constants import ANCHOR
 import requests
-from bs4 import BeautifulSoup
-from tkinter import Label
-from tkinter import Tk
-from PIL import ImageTk, Image
+from tkinter.font import BOLD
+from PIL import Image,ImageTk #pip install pillow
 
-url="https://weather.com"#use this link and select your own country and paste the same address
+root=tk.Tk()
+root.title("Weather App")
+root.geometry("580x400")
 
-master = Tk()
-master.title=("Weather App")
-master.config(bg="white")
+def format_response(weather):
+    try:
+        city=weather['name']
+        condition=weather['weather'][0]['main']
+        temp=weather['main']['temp']
+        humidity=weather['main']['humidity']
+        country=weather['sys']['country']
+        final_str='City : %s\nCondition : %s\nTemperature : %s\nHumidity : %s\nCountry: %s'%(city,condition,temp,humidity,country)
+    except:
+        final_str='Cannot show the data due to some error'
+    return final_str
 
-img = Image.open("---the image path---")#copy paste your image path
-img = img.resize((150,150))
-img = ImageTk.PhotoImage(img)
+def weather1(city):
+    weather_key='e299a13093bb78424d806adc86ba1adf' #if its not working you can use your own API keys from the link given in READ.ME
+    url='https://api.openweathermap.org/data/2.5/weather'
+    params={'APPID':weather_key,'q':city,'units':'imperial'}
+    response=requests.get(url,params)
+    weather=response.json()
+    result['text']=format_response(weather)
+    icon_name=weather['weather'][0]['icon']
+    open_img(icon_name)
 
-def getWeather():
-    page= requests.get(url)
-    soup= BeautifulSoup(page.content, "html.parser")
-    location= soup.find('h1', class_= "CurrentConditions--location--kyTeL").text
-    temperature = soup.find('span', class_= "CurrentConditions--tempValue--3a50n").text
-    weatherPrediction= soup.find('div', class_= "CurrentConditions--phraseValue--2Z18W").text
-    locationLable.config(text= location)
-    temperatureLable.config(text= temperature)
-    weatherPredictionLabel.config(text= weatherPrediction)
+def open_img(icon):
+    size=int(frame_two.winfo_height()*0.6)
+    img=ImageTk.PhotoImage(Image.open('---IMG PATH---'+icon+'.png').resize((size,size)))
+    weathericon.delete('all')
+    weathericon.create_image(0,0,anchor='nw',image=img)
+    weathericon.image=img
 
-locationLable= Label(master, font=("Sans Seriff", 20, "bold"))
-locationLable.grid(row=0, sticky="N", padx=100)
+img=Image.open("---IMG PATH---")
+img=img.resize((600,420))
+img_photo=ImageTk.PhotoImage(img)
 
-temperatureLable= Label(master, font=("Sans Seriff", 80, "bold"))
-temperatureLable.grid(row=1, sticky="W", padx=30, pady=0)
+bg_lbl=tk.Label(root,image=img_photo)
+bg_lbl.place(x=0,y=0,width=600,height=420)
 
-Label(master, image=img, bg="White").grid(row=1, sticky="E")
-weatherPredictionLabel= Label(master, font=("Sans Seriff", 15, "bold"), bg="white")
-weatherPredictionLabel.grid(row=2, sticky="W", padx=30, pady=0)
+heading_title=tk.Label(bg_lbl,text='Weather App GUI',bg='#D7E9F7',font=('poppins,sans-serif',30))
+heading_title.place(x=128,y=18)
 
-getWeather()
-master.mainloop()
+frame_one=tk.Frame(bg_lbl,bg="#08D9D6",bd=5)
+frame_one.place(x=45,y=80,width=490,height=50)
+
+txt_box=tk.Entry(frame_one,font=('poppins,sans-serif',23),width=20)
+txt_box.grid(row=0,column=0,sticky='W')
+
+btn=tk.Button(frame_one,text='Search',font=('poppins,sans-serif',15),width=11,command=lambda:weather1(txt_box.get()))
+btn.grid(row=0,column=1,padx=5)
+
+frame_two=tk.Frame(bg_lbl,bg="#FF2E63",bd=5)
+frame_two.place(x=45,y=150,width=490,height=232)
+
+result=tk.Label(frame_two,font=('poppins,sans-serif',22),justify='left',anchor='nw')
+result.place(relwidth=1,relheight=1)#relation for parent class
+
+weathericon=tk.Canvas(result,bd=0,highlightthickness=0)
+weathericon.place(relx=.75,rely=0,relwidth=2,relheight=2)
+
+root.mainloop()
